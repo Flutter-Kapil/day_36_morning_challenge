44e4823394c038cab29dee09054a25a339493fd1
kapil jhajhria <jhajhria44@gmail.com>
2019-11-22 09:51:35 +0530

    2 test cases now edge case: one contains same name  but different email id.

--- test/Day_36_Morning_Challenge_test.dart
+++ test/Day_36_Morning_Challenge_test.dart
@@ -2,7 +2,12 @@ import '../bin/main.dart';
 import 'package:test/test.dart';
 void main() {
   test("test case 1", () {
-	expect(merge([["John", "johnsmith@mail.com", "john00@mail.com"],
+// 	expect(merge([["John", "johnsmith@mail.com", "john00@mail.com"],
+// ["John", "johnnybravo@mail.com"], ["John", "johnsmith@mail.com", "john_newyork@mail.com"],
+// ["Mary", "mary@mail.com"]]), [["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'],
+//  ["John", "johnnybravo@mail.com"], ["Mary", "mary@mail.com"]]);
+
+ expect(merge([["John", "johnsmith@mail.com", "john00@mail.com"],
  ["John", "johnsmith@mail.com", "john_newyork@mail.com"],
 ["Mary", "mary@mail.com"]]), [["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'],
   ["Mary", "mary@mail.com"]]);
